ff2d8997f09a343ef5946c1194762076162d1c48
anaso <anyan_anaso@hotmail.co.jp>
2013-12-13 23:10:43 +0900

    Create MainClass and ymlFile

---
+++ GrowForest.java
@@ -0,0 +1,15 @@
+package zako02anaso.GrowForest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GrowForest extends JavaPlugin
+{
+	public void onEnable()
+	{
+		System.out.println("GrowForest In Bukkit");
+	}
+	
+	public void onDisable()
+	{
+	}
+}
