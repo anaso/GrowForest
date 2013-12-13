041cd274840694cb158e87a80505be7d3bc425a9
anaso <anyan_anaso@hotmail.co.jp>
2013-12-14 00:44:30 +0900

    アイテムがスポーンした時に判別を行い、苗木だったらサーバーログに名前とダメージ値を表示する。 苗木のダメージ値の取得は非推奨の関数を使用中。

--- GrowForest.java
+++ GrowForest.java
@@ -7,6 +7,8 @@ public class GrowForest extends JavaPlugin
 	public void onEnable()
 	{
 		System.out.println("GrowForest In Bukkit");
+		
+		new SaplingDespawnListener(this);
 	}
 	
 	public void onDisable()
